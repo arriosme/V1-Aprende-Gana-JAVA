f86bd71148c009a5de09ba1dec780a3e6882f43e
arriosme <arios@novopangea.com.co>
2022-06-12 22:59:46 -0500

    Antony debe elaborar la clase Second-Constants

---
+++ src/com/gameapp/utility/ConstantsUtility.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Esta clase la debe construir Antony
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gameapp.utility;
+
+/**
+ *
+ * @author arley
+ */
+public class ConstantsUtility {
+    
+}
